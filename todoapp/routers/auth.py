c23afb2162ca2f518e902e614154ee1893022015
RutamBhagat <rutambhagat@gmail.com>
2024-02-21 18:08:03 +0530

    add me endpoint

--- todoapp/routers/auth.py
+++ todoapp/routers/auth.py
@@ -1,14 +1,17 @@
 import os
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 from pydantic import BaseModel
 from models import Users
 from routers.todos import db_dependency
 from passlib.context import CryptContext
-from jose import jwt
+from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 def get_password_hash(password):
@@ -17,6 +20,31 @@ def get_password_hash(password):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(db_dependency)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid authentication credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(
+            token,
+            os.environ.get("SECRET_KEY"),
+            algorithms=[os.environ.get("ALGORITHM")],
+        )
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        user = db.query(Users).filter(Users.username == username).first()
+        if user is None:
+            raise credentials_exception
+        return user
+    except JWTError:
+        raise credentials_exception
 
 
 router = APIRouter()
@@ -116,3 +144,8 @@ async def login(db: db_dependency, user: UserLoginRequest):
 
     # Return the token
     return {"access_token": token, "token_type": "bearer"}
+
+
+@router.get("/me")
+async def read_users_me(current_user: Users = Depends(get_current_user)):
+    return current_user
