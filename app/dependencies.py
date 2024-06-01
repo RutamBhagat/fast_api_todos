ca24678fc75fd072ee0a018b2aa708f012d49f17
RutamBhagat <rutambhagat@gmail.com>
2024-06-02 01:36:45 +0530

    fix login issue

--- app/dependencies.py
+++ app/dependencies.py
@@ -4,9 +4,8 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 from app.auth.auth import get_current_user
 from app.db.database import get_db
-from app.auth.auth import oauth2_scheme
 
 
-login_dependency = Annotated[OAuth2PasswordRequestForm, Depends(oauth2_scheme)]
+login_dependency = Annotated[OAuth2PasswordRequestForm, Depends()]
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[Session, Depends(get_current_user)]
