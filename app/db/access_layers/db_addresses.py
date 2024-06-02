7cfee0ca1a38e2f11fee46c70958b02f8717b4e8
RutamBhagat <rutambhagat@gmail.com>
2024-06-02 16:53:51 +0530

    update address route

--- app/db/access_layers/db_addresses.py
+++ app/db/access_layers/db_addresses.py
@@ -3,22 +3,10 @@ from app.db.models import DBAddresses, DBUsers
 from app.db.schema import AddressBody
 
 
-# create a new address
-async def create_address(
-    db: Session, address: AddressBody, user: DBUsers
-) -> DBAddresses:
-    # delete the old address
-    old_address = (
-        db.query(DBAddresses).filter(DBAddresses.id == user.address_id).first()
-    )
-    if old_address is not None:
-        db.delete(old_address)
-
-    address = DBAddresses(**address.model_dump())
+# create a new address and upate the user's address_id
+async def create_address(db: Session, address_request: AddressBody, user: DBUsers):
+    address = DBAddresses(**address_request.model_dump())
     db.add(address)
-    db.flush()
-    user.address_id = address.id
-    db.add(user)
     db.commit()
-    db.refresh(address)
-    return address
+    user.address_id = address.id
+    db.commit()
--- app/dependencies.py
+++ app/dependencies.py
@@ -1,11 +1,13 @@
+from typing import Annotated
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordRequestForm
-from typing import Annotated
 from sqlalchemy.orm import Session
 from app.auth.auth import get_current_user
 from app.db.database import get_db
 
 
-login_dependency = Annotated[OAuth2PasswordRequestForm, Depends()] # NOTE: The Depends() does not need any params
+login_dependency = Annotated[
+    OAuth2PasswordRequestForm, Depends()
+]  # NOTE: The Depends() does not need any params
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[Session, Depends(get_current_user)]
